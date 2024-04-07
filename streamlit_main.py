6f3a394489470a2c178c0acb3730024cc2db8f32
haduchai <tankinguyen123@gmail.com>
2024-04-07 12:00:59 +0700

    add f

---
+++ streamlit_main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import joblib
+
+
+# Load model
+model = joblib.load('models/svm_project1.joblib')
+
+
+# Tiêu đề của ứng dụng
+st.title('Review Restaurant')
+
+# Hiển thị trình nhập dữ liệu để người dùng nhập số
+number = st.number_input(label='Nhập ID nhà hàng', type='int')
+
+# nhập dữ liệu comment
+comment = st.text_area(label='Nhập comment của bạn')
+
+# Kiểm tra xem người dùng đã nhập số hay chưa
+if st.button('Submit'):
+    st.write(f'id: {number}, comment: {comment}')
+    st.write('Đánh giá của bạn là: ' + str(model.predict([comment])[0]))
