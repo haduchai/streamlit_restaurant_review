199f277e1bbb95dc9ad0ebdf7cf9d5ea976b58f2
haduchai <tankinguyen123@gmail.com>
2024-04-07 15:34:27 +0700

    update

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ df = pd.read_csv('Data/2_Reviews.csv')
 st.title('Review Restaurant')
 
 # Hiển thị trình nhập dữ liệu để người dùng nhập số
-id = st.number_input(label='Nhập ID nhà hàng')
+id = st.number_input(label='Nhập ID nhà hàng', value=None, placeholder="Type a number...", step=1, min_value=1, max_value=1622)
 
 # nhập dữ liệu comment
 comment = st.text_area(label='Nhập comment của bạn')
@@ -29,7 +29,8 @@ comment = st.text_area(label='Nhập comment của bạn')
 if st.button('Submit'):
     # st.write(f'id: {number}, comment: {comment}')
     data = df[df['IDRestaurant'] == int(id)]
-    st.write('Đánh giá của bạn là: ' + str(model.predict([comment])[0]))
+    st.markdown("<h3 style='text-align: center;'> Đánh giá của bạn là: " + str(model.predict([comment])[0]) + "</h1>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: center;'>------------------------------------------------</h1>", unsafe_allow_html=True)
     # predict review
     pred = model.predict(data['Comment'])
     res = np.unique(pred, return_counts=True)
@@ -43,9 +44,9 @@ if st.button('Submit'):
     fig_hist.update_layout(title={'text':'Tổng quan các lượt ratings và phản hồi về nhà hàng', 'font': {'size': 30}}, xaxis={'title': 'Rating'}, yaxis={'title': 'Số lượt đánh giá'})
 
     # pie chart
-    fig_pie = [go.Pie(labels=res[0], values=res[1])]
+    fig_pie = [go.Pie(labels=res[0], values=res[1], marker={'colors': ['#1f77b4', '#ff7f0e', '#2ca02c']})]
     fig_pie = go.Figure(fig_pie)
-    fig_pie.update_layout(title={'text':'Tỉ lệ các loại đánh giá', 'font': {'size': 30}})
+    fig_pie.update_layout(title={'text':'Tỉ lệ các loại đánh giá về nhà hàng', 'font': {'size': 30}})
 
     # wordcloud negative
     text_neg = ' '.join(data[data['pred'] == 'Negative']['Comment'])
@@ -53,7 +54,7 @@ if st.button('Submit'):
     wc_neg = WordCloud(width=800, height=400, background_color='white').generate(text_neg)
     fig_wc_neg = plt.figure(figsize=(10, 5))
     plt.imshow(wc_neg, interpolation='bilinear')
-    plt.title('Wordcloud negative', fontsize=20, fontweight='bold')
+    plt.title('Wordcloud các đánh giá tiêu cực\n\n', fontsize=20, fontweight='bold')
     plt.axis('off')
     
     # wordcloud positive
@@ -61,14 +62,13 @@ if st.button('Submit'):
     text_pos = pt.optimized_process_text(text_pos, pt.stopwords_lst)
     wc_pos = WordCloud(width=800, height=400, background_color='white').generate(text_pos)
     fig_wc_pos = plt.figure(figsize=(10, 5))
-    plt.imshow(wc_neg, interpolation='bilinear')
-    plt.title('Wordcloud positive', fontsize=20, fontweight='bold')
+    plt.imshow(wc_pos, interpolation='bilinear')
+    plt.title('Wordcloud các đánh giá tích cực\n\n', fontsize=20, fontweight='bold')
     plt.axis('off')
     
     # display
     st.plotly_chart(fig_hist, use_container_width=True)
     st.plotly_chart(fig_pie, use_container_width=True)
-    st.pyplot(fig_wc_neg, use_container_width=True)
     st.pyplot(fig_wc_pos, use_container_width=True)
-
-
+    st.pyplot(fig_wc_neg, use_container_width=True)
+    
